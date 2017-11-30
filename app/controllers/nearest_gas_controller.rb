c2820680055b02768dcf09c9509b0f0de53c2c3b
joy <jg4743@nyu.edu>
2017-11-30 17:00:30 -0500

    Add params checking

--- app/controllers/nearest_gas_controller.rb
+++ app/controllers/nearest_gas_controller.rb
@@ -1,4 +1,18 @@
 class NearestGasController < ApplicationController
   def index
+    permitted = params.permit(:lat, :lng)
+    lat = permitted[:lat]
+    lng = permitted[:lng]
+    unless lat && lng
+      render json: {
+        error: 'params are invalid',
+        status: 400
+      }, status: 400
+      return
+    end
+    render json: {
+        message: 'hello',
+        status: 200
+      }, status: 200
   end
 end
