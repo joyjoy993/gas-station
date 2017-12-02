28c40f83ef22a60fa3c1e14923b8f38654ead0c7
joyjoy993 <jg4743@nyu.edu>
2017-12-02 17:07:10 -0500

    Move logic from C to M
      1. move logic from controller to model
      2. change name of collection to 'location'
      3. put some helper functions into concern
      4. add fetch cached gas station

---
+++ app/models/concerns/location_concern.rb
@@ -0,0 +1,90 @@
+module LocationConcern
+  extend ActiveSupport::Concern
+
+  GOOGLE_ADDRESS_COMPONENT_MAPPING_KEYS = {
+    street_number: {
+      original_key: 'long_name',
+      mapping_key: 'street_number'
+    },
+    route: {
+      original_key: 'long_name',
+      mapping_key: 'route'
+    },
+    locality: {
+      original_key: 'long_name',
+      mapping_key: 'city'
+    },
+    administrative_area_level_1: {
+      original_key: 'short_name',
+      mapping_key: 'state'
+    },
+    postal_code: {
+      original_key: 'long_name',
+      mapping_key: 'postal_code'
+    },
+    postal_code_suffix: {
+      original_key: 'long_name',
+      mapping_key: 'postal_code_suffix'
+    }
+  }
+
+  def concat_postal_code_or_address_string_helper(parsed_address_components, pattern, first_key, second_key)
+    formatted_string = ''
+    if parsed_address_components.key?(second_key) && parsed_address_components[second_key] != ''
+      formatted_string = format(
+                          pattern,
+                          parsed_address_components[first_key],
+                          parsed_address_components[second_key])
+    else
+      formatted_string = parsed_address_components[first_key]
+    end
+    return formatted_string
+  end
+
+  # A function to parse address components from google api
+  def parse_address_components_from_google_api(address_components)
+    parsed_address_components = {}
+    address_components.each{ |address_component|
+      types = address_component['types']
+      parse_address_components_helper(
+        types,
+        parsed_address_components,
+        address_component)
+    }
+    postal_code = concat_postal_code_or_address_string_helper(
+                    parsed_address_components,
+                    '%s-%s',
+                    'postal_code',
+                    'postal_code_suffix')
+    street_address = concat_postal_code_or_address_string_helper(
+                      parsed_address_components,
+                      '%s %s',
+                      'street_number',
+                      'route')
+    address = {
+      streetAddress: street_address,
+      city: parsed_address_components['city'],
+      state: parsed_address_components['state'],
+      postalCode: postal_code
+    }
+    return address
+  end
+
+  def parse_address_components_helper(types, parsed_address_components, address_component)
+    types.each{ |type|
+      type_symbol = type.to_sym
+      if GOOGLE_ADDRESS_COMPONENT_MAPPING_KEYS.include? type_symbol
+        mapping_key = GOOGLE_ADDRESS_COMPONENT_MAPPING_KEYS[type_symbol][:mapping_key]
+        original_key = GOOGLE_ADDRESS_COMPONENT_MAPPING_KEYS[type_symbol][:original_key]
+        parsed_address_components[mapping_key] = address_component[original_key]
+      end
+    }
+  end
+
+  def format_url_and_return_json_response(base_url, *params)
+    formatted_url = URI.encode(base_url % params)
+    response = open(formatted_url).read
+    return JSON.parse(response)
+  end
+
+end
---
+++ app/models/location.rb
@@ -0,0 +1,69 @@
+class Location
+  include LocationConcern
+  include Mongoid::Document
+  field :gps, type: Array
+  field :address, type: Hash
+  field :nearest_gas_station, type: Hash
+  field :query_time, type: DateTime, default: DateTime.now
+
+  validates :gps, presence: true
+
+  index({gps: '2d'}, {min: -180, max: 180})
+
+  CACHING_PRECISION = 0.005 / 111.12 # 5 meters
+  STALE_TIME = 3.days.ago # expire time
+  GOOGLE_MAP_KEY = Rails.application.secrets.google_api_key
+  REVERSE_GPS_QUERY_URL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s&key=%s'
+  GAS_STATION_QUERY_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s,%s&type=gas_station&rankby=distance&key=%s'
+  GEOCODING_QUERY_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'
+
+  def fetch_cache_gas_station(lat, lng)
+    gps = Array[lng.to_f, lat.to_f]
+    caching_result = Location.where(:query_time.gte => STALE_TIME)
+                      .geo_near(gps).max_distance(CACHING_PRECISION)
+    unless caching_result.empty?
+      return caching_result.first[:nearest_gas_station]
+    else
+      return nil
+    end
+  end
+
+  def fetch_nearest_gas_station(lat, lng)
+    gas_station_address = fetch_cache_gas_station(lat, lng)
+    if gas_station_address
+      return gas_station_address
+    end
+    response_from_gas_station_query = format_url_and_return_json_response(
+                                        GAS_STATION_QUERY_URL,
+                                        lat,
+                                        lng,
+                                        GOOGLE_MAP_KEY)
+    gas_station_address = response_from_gas_station_query['results'].first['vicinity']
+    response_from_geocoding_query = format_url_and_return_json_response(
+                                      GEOCODING_QUERY_URL,
+                                      gas_station_address,
+                                      GOOGLE_MAP_KEY)
+    address_components = response_from_geocoding_query['results'].first['address_components']
+    return parse_address_components_from_google_api(address_components)
+  end
+
+  def fetch_address(lat, lng)
+    addresses = []
+    response_from_reverse_gps_query = format_url_and_return_json_response(
+                                        REVERSE_GPS_QUERY_URL,
+                                        lat,
+                                        lng,
+                                        GOOGLE_MAP_KEY)
+    response_from_reverse_gps_query['results'].each { |result|
+      address = parse_address_components_from_google_api(result['address_components'])
+      # When an address doesn't have streetAddress, it's a general address like a state, a place or a area marker
+      next if address[:streetAddress].nil?
+      addresses.push({
+        address: address
+      })
+    }
+    address = addresses.empty? ? nil : addresses.first[:address]
+    return address
+  end
+
+end
