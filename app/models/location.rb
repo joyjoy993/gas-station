6ea0026c700a88c6d46938ca392bb6de09eb6195
joy <jg4743@nyu.edu>
2017-12-05 12:11:34 -0500

    Validate gps in location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,7 +6,19 @@ class Location
   field :query_time, type: DateTime, default: DateTime.now
 
   validates :gps, :query_time, :address, :nearest_gas_station, presence: true
+  validate :gps_valid?
 
   index({gps: '2d'}, {min: -180, max: 180, unique: true})
+
+  private
+
+  def gps_valid?
+    lat = gps[1]
+    lng = gps[0]
+    location = NearestGasValidators::GpsValidator.new(lat, lng)
+    unless location.valid?
+      errors.add(:gps, 'Invalid gps pair')
+    end
+  end
 
 end
